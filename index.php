074252e51b7b82b519bb3890c01b1b2adb137740
Jan Croonen <info@jcroonen.nl>
2013-03-01 13:10:20 +0100

    bug not fixed
    playById werkt niet meer op dit object

--- index.php
+++ index.php
@@ -1,29 +1,35 @@
 <!DOCTYPE html>
+<?php
+// youtubeIframePlayer/index.php
+
+$url = isset($_REQUEST['url']) ? $_REQUEST['url'] : '';
+$start = 'http://www.youtube.com/embed/u1zgFlCw8Aw?origin=http://yplayer&enablejsapi=1&autohide=1';
+//$start = '';
+?>
 <html>
   <head>
-
-
     <link type="text/css" rel="stylesheet" href="y.css">
-
   </head>
   <body>
     <!-- 1. The <iframe> (and video player) will replace this <div> tag. -->
-    <div id="player"></div>
-
-    <!-- droppable area -->
+    <!--div id="player"></div-->
+    <iframe id="player" type="text/html"
+            src="<?=$start ?>"
+            frameborder="0">
+    </iframe>
     <div id="controls">
       <label for="setStart">seek</label>
       <input type="text" id="setStart">
+      <span id="currentTime"></span>
     </div>
+    <!-- droppable area -->
     <div id="droparea"></div>
-
     <script>
-      var YPLAYER_url = "<?= $_REQUEST['url'] ?>";
+      var YPLAYER_url = "<?= $url?>";
     </script>
     <script src="//ajax.googleapis.com/ajax/libs/jquery/1.8.3/jquery.min.js"></script>
     <script src="jquery.droppable.js"></script>
     <script src="keyevent.js"></script>
     <script type="text/javascript" src="y.js"></script>
-
   </body>
 </html>
--- y.js
+++ y.js
@@ -1,6 +1,6 @@
 /*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
+ * Use youtube api to create a player and open the url.
+ * api: see https://developers.google.com/youtube/js_api_reference
  */
 // 2. This code loads the IFrame Player API code asynchronously.
 var tag = document.createElement('script');
@@ -8,34 +8,49 @@ tag.src = "//www.youtube.com/iframe_api";
 var firstScriptTag = document.getElementsByTagName('script')[0];
 firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
 
-// 3. This function creates an <iframe> (and YouTube player)
-//    after the API code downloads.
 var player,
     dropHeight = 100,
-    playerMargin = 40,
-    dimensions = [];
+    playerMargin = 20,
+    dimensions = [],
+    timeoutLoad = 1000;
 
-var getIdFromUrl = function(url) {
-  var urlstart = 'www.youtube.com/watch?v=',
-      pos = url.indexOf(urlstart);
-
-  if (pos != -1) {
-    url = url.substr(pos + urlstart.length);
-    pos = url.indexOf('&');
-    if (pos != -1) {
-      url = url.substr(0, pos);
-    }
-    return url;
-  }
-  return null;
-};
-
-function getDimensions() {
+var getDimensions = function() {
   var w = document.documentElement.clientWidth - playerMargin,
       h = w / 640 * 390;
 
   return {w: w, h: h};
 }
+
+var getParms = function(s) {
+  //console.log(s);
+  var pp = s.split('_and_'),
+      parms = {};
+
+  for (var i = 0; i < pp.length; i++) {
+    var z = pp[i].split('=');
+    parms[z[0]] = z[1];
+  }
+  return parms;
+}
+
+var getIdFromUrl = function(url) {
+  // bijv. http://www.youtube.com/watch?feature=player_embedded&v=6TMUCzQwoZQ
+  var urlstart = 'www.youtube.com/watch?',
+      pos = url.indexOf(urlstart);
+
+  if (pos != -1) {
+    // get querystring, bijv. feature=player_embedded&v=6TMUCzQwoZQ
+    var qs = url.substr(pos + urlstart.length);
+    var parms = getParms(qs);
+    //console.log(parms);
+    //alert(parms['v']);
+    if (parms['v']) {
+      //console.log(parms);
+      return parms['v'];
+    }
+  }
+  return null;
+};
 
 function onYouTubeIframeAPIReady() {
   var dim = getDimensions();
@@ -46,26 +61,66 @@ function onYouTubeIframeAPIReady() {
     height: dim['h'], //'390',
     videoId: 'u1zgFlCw8Aw'
   });
+  if (typeof player=='undefined') return;
+
+//console.log(player);
+
+  //player.setSize(dim['w'], dim['h']);
 
   // querystring?
   if (YPLAYER_url) {
     var id = getIdFromUrl(YPLAYER_url);
+    //if (id == null) id =  getIdFromV(YPLAYER_url);
+    //console.log(YPLAYER_url);
+      //  console.log(id);
 
     if (id != null) {
       setTimeout(function(){
-        player.loadVideoById(id);
-      }, 500);
+        player.loadVideoById({
+          videoId: id,
+          //endSeconds: 5,
+          suggestedQuality: 'hd1080'  // 'large' = 480px
+        });
+        player.pauseVideo();
+      }, timeoutLoad);
     }
   }
 }
 
 $(function() {  // on ready
+
+  // Parse timestring, e.g. 1:02:45.
   var getSeconds = function(v) {
     var w = v.split(":");
 
+    // minuten en seconden
     if (w.length == 2) return w[0] * 60 + w[1] * 1;
+
+    // uren, minuten en seconden
     else if (w.length == 3) return w[0] * 3600 + w[1] * 60 + w[2] * 1;
+
+    // alleen minuten
     else return v * 60; // default: minutes
+  };
+
+  // Format time, given in seconds; e.g. 1:02:45.
+  var getReadableTime = function(sec) {
+
+    var hours, minutes, seconds = sec;
+    var oHours, oMinutes, oSeconds;
+
+    hours = parseInt(seconds / 3600);
+    seconds = seconds - (hours * 3600);
+    minutes = parseInt(seconds / 60);
+    seconds = parseInt(seconds - (minutes * 60));
+
+    oMinutes = minutes < 10? '0' + minutes : minutes;
+    oSeconds = seconds < 10? '0' + seconds : seconds;
+
+    oHours = hours > 0? hours + ':' : '';
+    oMinutes = oMinutes + ':';
+
+    return oHours + oMinutes + oSeconds;
   };
 
   // make area droppable
@@ -79,7 +134,7 @@ $(function() {  // on ready
   });
   var resizeTimer;
 
-  // adapt player when resizing window
+  // Adapt size of player when resizing window.
   $(window).resize(function() {
     var dim = getDimensions();
 
@@ -89,7 +144,7 @@ $(function() {  // on ready
     }, 100);
   });
 
-  // seekTo
+  // seekTo: go to time.
   $('#setStart').keydown(function(e){
     if (e.keyCode == KeyEvent.DOM_VK_RETURN) {
       var v = $(this).val(),
@@ -100,4 +155,10 @@ $(function() {  // on ready
     }
   });
 
+  // Display current time every second.
+  setInterval(function(){
+    if (player.getPlayerState && player.getPlayerState() == 1) { // playing
+      $('#currentTime').html(getReadableTime(player.getCurrentTime()));
+    }
+  },1000);
 })
